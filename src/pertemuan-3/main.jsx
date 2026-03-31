4c9b0ad73c9e9264c064aa9318178f3ba63d0886
alhafizh24si <alhafizh24si@mahasiswa.pcr.ac.id>
2026-03-31 16:24:01 +0700

    User Form & HitungGajiForm

--- src/pertemuan-3/main.jsx
+++ src/pertemuan-3/main.jsx
@@ -1,12 +1,16 @@
 import { createRoot } from "react-dom/client";
-import TailwindCSS from "./TailwindCSS";
+import TailwindCSS from "./TailwindCSS"
+import UserForm from "./UserForm";
 import "./tailwind.css";
+import HitungGajiForm from "./HitungGajiForm";
 
 
 createRoot(document.getElementById("root"))
     .render(
         <div>
-             <TailwindCSS/>
+             {/* <TailwindCSS/> */}
+                {/* <UserForm/> */}
+                <HitungGajiForm/>
         </div>
         
     )
