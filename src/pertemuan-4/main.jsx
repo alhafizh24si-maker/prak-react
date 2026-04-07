315fbe3fb9143ffbceebb5254ed56f386c8d10b9
alhafizh24si <alhafizh24si@mahasiswa.pcr.ac.id>
2026-04-07 14:46:33 +0700

    Daa Json

---
+++ src/pertemuan-4/main.jsx
@@ -0,0 +1,15 @@
+import { createRoot } from "react-dom/client";
+import FrameworkList from "./frameworkList";
+import "./tailwind.css";
+
+
+createRoot(document.getElementById("root"))
+    .render(
+
+        <div>
+             <FrameworkList/>
+             
+        </div>
+        
+    )
+    
