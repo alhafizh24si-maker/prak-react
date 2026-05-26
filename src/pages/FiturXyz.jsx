75fc741997551522bc7f5937a2e0921edab1c08b
alhafizh24si <alhafizh24si@mahasiswa.pcr.ac.id>
2026-05-26 15:28:08 +0700

    Fungsi Button Card Badge ShadCN

--- src/pages/FiturXyz.jsx
+++ src/pages/FiturXyz.jsx
@@ -1,3 +1,6 @@
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Badge } from 'lucide-react';
 import React from 'react';
 
 export default function FiturXyz() {
@@ -38,7 +41,36 @@ export default function FiturXyz() {
           </button>
         </div>
       </div>
+            <Button variant="" size="xs">Simpan</Button>
+            <Button variant="outline" size='sm'>Simpan</Button>
+            <Button variant="secondary">Simpan</Button>
+            <Button variant="ghost">Simpan</Button>
+            <Button variant="destructive">Simpan</Button>
+            <Button variant="link">Simpan</Button>
 
+        <Card className="mt-4 w-[380px]">
+        <CardHeader>
+          <div className="flex items-center justify-between">
+            <CardTitle>Belajar shadcn/ui</CardTitle>
+            <Badge variant="secondary">Baru</Badge>
+          </div>
+          <CardDescription>
+            Contoh penggunaan komponen shadcn/ui di React
+          </CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          <p className="text-sm text-muted-foreground">
+            Komponen ini dibuat di branch <strong>setup-shadcn</strong>
+            lalu di-merge ke main.
+          </p>
+        </CardContent>
+
+        <CardFooter className="flex gap-2">
+          <Button>Simpan</Button>
+          <Button variant="outline">Batal</Button>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
